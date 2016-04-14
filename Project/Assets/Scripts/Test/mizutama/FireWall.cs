0060e2cd083e59fa78b3cfa4c13b58900d6613a8
ryo0306 <iroha_0416@yahoo.co.jp>
2016-04-14 17:58:19 +0900

    火のギミックを追加

---
+++ Project/Assets/Scripts/Test/mizutama/FireWall.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWall : MonoBehaviour {
+
+    [SerializeField, Tooltip("火の本体,")]
+    GameObject _fire = null;
+
+    [SerializeField, Range(1, 5), Tooltip("何回でで火が消えるか")]
+    int _maxTouthCount = 1;
+    private int _touthCount = 0;
+
+    [SerializeField, Range(1f, 10f), Tooltip("何秒間火を消すかの最大時間")]
+    float _maxWaitTimeOfReturn = 1f;
+    // 火が消えている時間
+    private float _waitTimeOfReturn = 0f;
+
+    void Start()
+    {
+        _touthCount = _maxTouthCount;
+        _waitTimeOfReturn = _maxWaitTimeOfReturn;
+    }
+
+    void Update()
+    {
+        DeleteFire();
+        CountWaitTimeOfReturn();
+    }
+    private void CountWaitTimeOfReturn()
+    {
+        if (!_fire.activeInHierarchy)
+            _waitTimeOfReturn -= Time.deltaTime;
+
+        if (_waitTimeOfReturn <= 0)
+            ReBorn();
+    }
+
+    private void ReBorn()
+    {
+        _fire.SetActive(true);
+        _touthCount = _maxTouthCount;
+        _waitTimeOfReturn = _maxWaitTimeOfReturn;
+    }
+
+
+
+    private void DeleteFire()
+    {
+        _touthCount--;
+        if (_touthCount <= 0)
+            _fire.SetActive(false);
+    }
+}
