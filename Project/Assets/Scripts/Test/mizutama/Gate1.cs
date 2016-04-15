b1a7005fca3218815a90d477b8a49ac2939f0515
ryo0306 <iroha_0416@yahoo.co.jp>
2016-04-15 16:54:05 +0900

    ギミックの追加

---
+++ Project/Assets/Scripts/Test/mizutama/Gate1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gate1 : MonoBehaviour {
+
+
+    [SerializeField, Tooltip("スイッチの本体")]
+    private GameObject _windMill = null;
+
+    [SerializeField, Range(0.0f, 1.0f), Tooltip("開く速度")]
+    float _upSpeed = 0.01f;
+    [SerializeField,Tooltip("どこまで開くか(座標は親との差)")]
+    public float _maxPosition = 5.14f;
+    [SerializeField, Tooltip("どこまで閉じるか(座標は親との差)")]
+    public float _minPosition = 0.0f;
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (_windMill.GetComponent<WindMill>().GetOpen()) 
+        if(transform.position.y < _maxPosition)
+        transform.position +=new Vector3(0, _upSpeed, 0);
+
+        if(!_windMill.GetComponent<WindMill>().GetOpen())
+            if(transform.position.y > _minPosition)
+                transform.position -= new Vector3(0, _upSpeed, 0);
+    }
+}
--- Project/Assets/Scripts/Test/mizutama/MovePropeller.cs
+++ Project/Assets/Scripts/Test/mizutama/MovePropeller.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MovePropeller : MonoBehaviour
 {
-    [SerializeField, Range(0.0f, 3.0f), Tooltip("回転の最高速度,")]
+    [SerializeField, Range(0.0f, 5.0f), Tooltip("回転の最高速度,")]
     float _maxRotateSpeed = 0.5f;
 
     [SerializeField, Range(0.0f, 3.0f), Tooltip("回転の最低速度")]
@@ -18,14 +18,16 @@ public class MovePropeller : MonoBehaviour
     //復活する時間
     private float _waitTimeOfReturn = 0.0f;
 
-    [SerializeField, Range(1, 50), Tooltip("何回タッチしたら最低速度になるか,")]
+    [SerializeField, Range(0.0f, 10.0f), Tooltip("何秒ごとに加速するか")]
+    float _maxWaitTimeOfAcceleration = 0.0f;
+
+    private float _waitTimeOfAcceleration = 0.0f;
+
+    [SerializeField, Range(1, 10), Tooltip("何回タッチしたら最低速度になるか,")]
     int _maxtouthCount = 1;
 
     //タッチの回数
     private int _touthCount = 0;
-
-    //初期状態の時にタッチしたかどうか
-    private bool _firstTouth;
 
 
     void Start()
@@ -35,44 +37,43 @@ public class MovePropeller : MonoBehaviour
 
     void FixedUpdate()
     {
-        DeleyRotate();
-        CountReBornTime();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (!Physics.Raycast(ray, out hit)) return;
+            if (hit.collider.gameObject.name != transform.name) return;
+            DeleyRotate();
+        }
+        CountAccelerationTime();
         transform.Rotate(new Vector3(0, 0, _rotateSpeed * Time.deltaTime * 100));
 
     }
 
-    //復活時間を測定
-    private void CountReBornTime()
-    {
-        if (!_firstTouth) return;
-        _waitTimeOfReturn += Time.deltaTime;
 
-        if (_waitTimeOfReturn < _maxWaitTimeOfReturn) return;
-        ReBorn();
+    private void CountAccelerationTime()
+    {
+        _waitTimeOfAcceleration += Time.deltaTime;
+
+        if (_waitTimeOfAcceleration < _maxWaitTimeOfAcceleration) return;
+        _waitTimeOfAcceleration = 0.0f;
+
+        if (_rotateSpeed > _maxRotateSpeed) return;
+        _rotateSpeed += (_maxRotateSpeed - _minRotateSpeed) / _maxtouthCount;
+
+        if (_touthCount < 0) return;
+        --_touthCount;
     }
 
-    //復活
-    private void ReBorn()
-    {
-        _waitTimeOfReturn = 0;
-        _rotateSpeed = _maxRotateSpeed;
-        _firstTouth = false;
-        _touthCount = 0;
-    }
+
 
     //タッチしたら動きが遅くなる
     private void DeleyRotate()
     {
-        if(!_firstTouth)
-        _firstTouth = true;
-
-        if (_touthCount > _maxtouthCount) return;
-        ++_touthCount;
-
-
+        
         //規定のタッチしたら最低速度になるように処理
-        if (!_firstTouth) return;
-        if (_touthCount > _maxtouthCount) return;
+        if (_touthCount >= _maxtouthCount) return;
+        ++_touthCount;
         _rotateSpeed -= (_maxRotateSpeed - _minRotateSpeed) / _maxtouthCount;
 
     }
