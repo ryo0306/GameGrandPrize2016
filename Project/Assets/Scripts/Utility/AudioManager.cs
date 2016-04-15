ff748db806200e485656584d991472b4fa764bd2
zun-ferrum <dry.tm.fe@icloud.com>
2016-04-15 12:34:19 +0900

    RayCast Audio処理追加

---
+++ Project/Assets/Scripts/Utility/AudioManager.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AudioManager : MonoBehaviour
+{
+    //BGMのList
+    public List<AudioClip> BGMList;
+    //SEのList
+    public List<AudioClip> SEList;
+
+    public int MaxSE = 10;
+
+    private AudioSource bgmSource = null;
+    private List<AudioSource> seSources = null;
+    private Dictionary<string, AudioClip> bgmDict = null;
+    private Dictionary<string, AudioClip> seDict = null;
+
+
+    static GameObject _instance = null;
+
+    public void Awake()
+    {
+        if (_instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = gameObject;
+        }
+
+        else
+        {
+            Destroy(this);
+        }
+        //create listener
+        if (FindObjectsOfType(typeof(AudioListener)).All(o => !((AudioListener)o).enabled))
+        {
+            this.gameObject.AddComponent<AudioListener>();
+        }
+        //create audio sources
+        this.bgmSource = this.gameObject.AddComponent<AudioSource>();
+        this.seSources = new List<AudioSource>();
+
+        //create clip dictionaries
+        this.bgmDict = new Dictionary<string, AudioClip>();
+        this.seDict = new Dictionary<string, AudioClip>();
+
+        Action<Dictionary<string, AudioClip>, AudioClip> addClipDict = (dict, c) =>
+        {
+            if (!dict.ContainsKey(c.name))
+            {
+                dict.Add(c.name, c);
+            }
+        };
+
+        this.BGMList.ForEach(bgm => addClipDict(this.bgmDict, bgm));
+        this.SEList.ForEach(se => addClipDict(this.seDict, se));
+    }
+
+    public void PlaySE(string seName)
+    {
+        if (!this.seDict.ContainsKey(seName)) throw new ArgumentException(seName + " not found", "seName");
+
+        AudioSource source = this.seSources.FirstOrDefault(s => !s.isPlaying);
+        if (source == null)
+        {
+            if (this.seSources.Count >= this.MaxSE)
+            {
+                Debug.Log("SE AudioSource is full");
+                return;
+            }
+
+            source = this.gameObject.AddComponent<AudioSource>();
+            this.seSources.Add(source);
+        }
+
+        source.clip = this.seDict[seName];
+        source.Play();
+    }
+
+    public void StopSE()
+    {
+        this.seSources.ForEach(s => s.Stop());
+    }
+
+    public void PlayBGM(string bgmName, bool isLooping_)
+    {
+        if (!this.bgmDict.ContainsKey(bgmName)) throw new ArgumentException(bgmName + " not found", "bgmName");
+        if (this.bgmSource.clip != this.bgmDict[bgmName])
+        {
+            this.bgmSource.clip = this.bgmDict[bgmName];
+            this.bgmSource.Play();
+        }
+
+        if (!isLooping_) return;
+        if (!this.bgmSource.isPlaying)
+            this.bgmSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        this.bgmSource.Stop();
+        this.bgmSource.clip = null;
+    }
+
+
+
+}
---
+++ Project/Assets/Scripts/Utility/HitRayCast.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitRayCast : MonoBehaviour
+{ 
+    public bool HitRay(string hitObjectName_)
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if (!Physics.Raycast(ray, out hit)) return false;
+
+        if (hit.collider.gameObject.name != hitObjectName_) return false;
+        
+        return true;
+    }
+}
