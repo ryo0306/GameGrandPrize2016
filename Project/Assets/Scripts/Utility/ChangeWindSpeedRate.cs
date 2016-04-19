97ee4c199bbcd74fe4563c0ce28e1af267492194
zun-ferrum <dry.tm.fe@icloud.com>
2016-04-19 17:10:36 +0900

    ボイスによる移動処理のTest作成

---
+++ Project/Assets/Scripts/Utility/ChangeWindSpeedRate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 4 / 19
+/// 野本
+/// 作業開始
+/// </summary>
+public class ChangeWindSpeedRate : MonoBehaviour
+{
+    [SerializeField,Range(0.0f,50.0f),Tooltip("移動速度")]
+    float _moveSpeed = 5f;
+    [SerializeField, Range(0.0f, 5.0f), Tooltip("進行方向に対してMinus方向の加速度")]
+    float _minusMoveSpeed = 0.3f;
+
+    void Update()
+    {
+
+    }
+
+    void  FixedUpdate()
+    {
+        _moveSpeed += (GetComponent<MikeInput>().GetAveragedVolume() - _minusMoveSpeed);
+        if (_moveSpeed < 0f) _moveSpeed = 0f;
+        else if (_moveSpeed > 10f) _moveSpeed = 10f;
+        transform.localPosition += new Vector3(_moveSpeed * Time.deltaTime,0,0);
+    }
+}
--- Project/Assets/Scripts/Utility/MikeInput.cs
+++ Project/Assets/Scripts/Utility/MikeInput.cs
@@ -12,35 +12,48 @@ using System.Collections;
 
 public class MikeInput : MonoBehaviour
 {
+    private float _nowVol = 0f;
+
+    public float nowVolume
+    {
+        get { return _nowVol; }
+        //Setは外で使うことはないが、もしもの時に０にするために用意している
+        set { _nowVol = value; }
+    }
+
     void Start()
     {
-       
+
         GetComponent<AudioSource>().clip = Microphone.Start(null, true, 999, 44100);  // マイクからのAudio-InをAudioSourceに流す
-       
+
         GetComponent<AudioSource>().loop = true;                                      // ループ再生にしておく
 
-                                          
+
         GetComponent<AudioSource>().volume = 0.01f;                                   // MuteにするとVolを返さないので0.01にする
 
         while (!(Microphone.GetPosition("") > 0)) { }                                 // マイクが取れるまで待つ。空文字でデフォルトのマイクを探してくれる
         GetComponent<AudioSource>().Play();                                           // 再生する
     }
 
+
+    //Fixedのほうがいいのか質問する
     void Update()
     {
-        float vol = GetAveragedVolume();
-        Debug.Log(vol);
+        _nowVol = GetAveragedVolume();
     }
 
-    float GetAveragedVolume()
+    public float GetAveragedVolume()
     {
         float[] data = new float[256];
-        float a = 0;
+        float soundSize = 0;
         GetComponent<AudioSource>().GetOutputData(data, 0);
         foreach (float s in data)
         {
-            a += Mathf.Abs(s);
+            soundSize += Mathf.Abs(s);
         }
-        return a *  256.0f;
+        //Max値は１に設定
+        if (soundSize > 1f) soundSize = 1.0f;
+        else if (soundSize < 0.1f) soundSize = 0.0f;
+        return soundSize * 3f;
     }
 }
