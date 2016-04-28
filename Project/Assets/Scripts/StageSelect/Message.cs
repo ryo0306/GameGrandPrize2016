d524f3b23351fa5286fdea0c4546c5f15c0caec4
ryo0306 <iroha_0416@yahoo.co.jp>
2016-04-28 19:16:24 +0900

    StageSelectの追加

---
+++ Project/Assets/Scripts/StageSelect/Message.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Message : MonoBehaviour {
+
+    [SerializeField, Range(0.00f, 0.10f), Tooltip("変化する速さ")]
+    public float _changeSpeed;
+
+    private float _alpha;
+
+	
+	void Start () {
+        _alpha = 1.0f;
+	}
+	
+	
+	void Update () {
+        if (_alpha > 1.0f)
+            _changeSpeed = -_changeSpeed;
+
+        if (_alpha < 0.0f)
+            _changeSpeed = -_changeSpeed;
+
+        _alpha -= _changeSpeed;
+
+
+        this.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, _alpha);
+	}
+}
---
+++ Project/Assets/Scripts/StageSelect/StagePhotoEasing.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+    public class StagePhotoEasing : MonoBehaviour
+    {
+
+        [SerializeField, Tooltip("左端の最大")]
+        public float _leftMax;
+        [SerializeField, Tooltip("右端の最大")]
+        public float _rightMax;
+
+        float _animationTime;
+
+        public float _range;
+
+        public bool _slide;
+
+        private float _direction;
+
+        private float _startPos;
+        private readonly object tranceform;
+
+        void Start()
+        {
+            _animationTime = 0.0f;
+            _startPos = transform.localPosition.x;
+        }
+
+        void Update()
+        {
+
+            if (_slide)
+            {
+                _animationTime += 0.01f;
+
+                transform.localPosition = new Vector3(EasingCircInOut(_animationTime, _startPos, _startPos + _range * _direction), transform.localPosition.y, transform.localPosition.z);
+
+                if (_animationTime >= 1.0f)
+                {
+                    _animationTime = 0.0f;
+                    MoveLimit();
+                    _startPos = transform.localPosition.x;
+                    _slide = false;
+                }
+            Debug.Log(transform.localPosition.x);
+            }
+
+        }
+
+        //Easing関数
+        private float EasingCircInOut(float t, float b, float e)
+        {
+            if ((t /= 0.5f) < 1) return -(e - b) / 2 * (Mathf.Sqrt(1 - t * t) - 1) + b;
+            t -= 2;
+            return (e - b) / 2 * (Mathf.Sqrt(1 - t * t) + 1) + b;
+        }
+
+        public void RightSlideOn()
+        {
+            _slide = true;
+            _direction = 1.0f;
+        }
+
+        public void LeftSlideOn()
+        {
+            _slide = true;
+            _direction = -1.0f;
+        }
+
+        private void MoveLimit()
+        {
+            if (transform.localPosition.x > _rightMax)
+            transform.localPosition = new Vector3(_leftMax,transform.localPosition.y, transform.localPosition.z);
+
+            if (transform.localPosition.x < _leftMax)
+            transform.localPosition = new Vector3(_rightMax, transform.localPosition.y, transform.localPosition.z);
+        }
+    }
