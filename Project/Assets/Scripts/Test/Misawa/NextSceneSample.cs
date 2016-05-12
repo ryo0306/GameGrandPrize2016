692c4f13843c0aec96e0a618fe316ba2646c3f7f
hmrskw <hmrskw@gmail.com>
2016-05-13 00:54:21 +0900

    次のシーンへ移動するときの処理の追加

---
+++ Project/Assets/Scripts/Test/Misawa/NextSceneSample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextSceneSample : MonoBehaviour {
+
+    private GameObject _dataManager = null;
+
+    int _retryCount = 0;
+// Use this for initialization
+void Start () {
+        if ((_dataManager = GameObject.Find("DataManager")) == null)
+        {
+            Debug.Log("DataManagerが見つかりません");
+        }
+        else
+        {
+            _retryCount = _dataManager.GetComponent<Score>().GetCount();
+            Destroy(_dataManager);
+        }
+        Debug.Log(_retryCount);
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+}
