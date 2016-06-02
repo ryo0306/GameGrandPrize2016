df91f1aa10b619c33588c3c264b014f7d5e5d438
ryo0306 <iroha_0416@yahoo.co.jp>
2016-06-02 14:32:31 +0900

    birdの処理修正

--- Project/Assets/Scripts/Test/mizutama/BirdMove.cs
+++ Project/Assets/Scripts/Test/mizutama/BirdMove.cs
@@ -3,13 +3,46 @@ using System.Collections;
 
 public class BirdMove : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+
+
+    //メインカメラのタグ名　constは定数(絶対に変わらない値)
+    private const string MAIN_CAMERA_TAG_NAME = "MainCamera";
+    //カメラに映っているかの判定
+    private bool _isRendered = false;
+
 
     [SerializeField, Range(1, 50), Tooltip("移動速度,")]
     float _speed = 0.0f;
 
 
+    void OnTriggerEnter2D(Collider2D col)
+    {
+      
+        if (_isRendered)
+        {
+
+        }
+        
+    }
+
+    //Rendererがカメラに映ってる間に呼ばれ続ける
+    void OnWillRenderObject()
+    {
+        //メインカメラに映った時だけ_isRenderedをtrue
+        if (Camera.current.tag == MAIN_CAMERA_TAG_NAME)
+        {
+            _isRendered = true;
+        }
+    }
+
     void Update()
     {
-        transform.position -= new Vector3(_speed * Time.deltaTime, 0, 0);
+
+        if (_isRendered)
+        {
+            transform.position -= new Vector3(_speed * Time.deltaTime, 0, 0);    
+        }
+
     }
 }
